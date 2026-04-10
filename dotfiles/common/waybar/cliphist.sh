197623e33f7dc7f26468a0b208c511f87dde5019
WillGle <cuongsayyay@gmail.com>
2026-04-10 21:54:31 +0700

    refactor(desktop): migrate launcher and menus from wofi to rofi

--- dotfiles/common/waybar/cliphist.sh
+++ dotfiles/common/waybar/cliphist.sh
@@ -1,9 +1,2 @@
 #!/usr/bin/env bash
-
-cliphist list | \
-wofi \
-  --conf ~/.config/wofi/config-clip.ini \
-  --style ~/.config/wofi/style.css \
-  --show dmenu \
-  --prompt "Clipboard:" | \
-cliphist decode | wl-copy
+exec "$HOME/.local/bin/rofi-clipboard"
